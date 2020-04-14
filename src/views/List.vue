0111e9dac61312889c80b25babfb40d8a9911a4e
nakamurayukino <>
2020-04-14 09:47:56 +0900

    fix List.vue to select status

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,28 +1,93 @@
 <template>
-  <div class="home">
-    <h2>Add ToDo</h2>
-    <form @submit="addTodo">
-      <input v-model="newText" type="text" />
-      <button type="submit">作成</button>
-    </form>
-    <h2>ToDo</h2>
-    <ul>
-      <li v-for="todo in notDoneTodo" :key="todo.name">
-        <input type="checkbox" v-model="todo.isDone" />
-        {{ todo.name }}
-        <button @click="deleteTodo(todo)">削除</button>
-      </li>
-    </ul>
-    <h2>Done</h2>
-    <button @click="cleanTodo">Clean</button>
-    <ul>
-      <li v-for="todo in doneTodo" :key="todo.name">
-        <input type="checkbox" v-model="todo.isDone" />
-        {{ todo.name }}
-        <button @click="deleteTodo(todo)">削除</button>
-      </li>
-    </ul>
-  </div>
+  <v-app>
+    <div class="home">
+      <h2>Add ToDo</h2>
+      <form @submit="addTodo">
+        <input v-model="newText" type="text" />
+        <button type="submit">作成</button>
+      </form>
+      <h2>ToDo</h2>
+      <ul>
+        <v-container fluid>
+          <li v-for="t in todo" :key="t.name">
+            <v-row align="center">
+              <v-col class="d-flex" cols="12" sm="4">
+                <v-select
+                  :items="items"
+                  v-model="t.status"
+                  item-text="label"
+                  item-value="value"
+                  label="Status"
+                  return-object
+                  @change="changeStatus(t, $event)"
+                ></v-select>
+              </v-col>
+              <v-col class="d-flex" cols="12" sm="4">
+                <!-- <input type="checkbox" v-model="t.state===''" /> -->
+                {{ t.name }}
+              </v-col>
+              <v-col class="d-flex" cols="12" sm="4">
+                <button @click="deleteTodo(t)">削除</button>
+              </v-col>
+            </v-row>
+          </li>
+        </v-container>
+      </ul>
+      <h2>Doing</h2>
+      <button @click="cleanTodo">Clean</button>
+      <ul>
+        <li v-for="t in doingTodo" :key="t.name">
+          <v-row align="center">
+            <v-col class="d-flex" cols="12" sm="4">
+              <v-select
+                :items="items"
+                :value="t.status"
+                item-text="label"
+                item-value="value"
+                label="Status"
+                return-object
+                @change="changeStatus(t, $event)"
+              ></v-select>
+              <!-- 何に変えたのかって情報-->
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="4">
+              <!-- <input type="checkbox" v-model="t.state===''" /> -->
+              {{ t.name }}
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="4">
+              <button @click="deleteTodo(t)">削除</button>
+            </v-col>
+          </v-row>
+        </li>
+      </ul>
+      <h2>Done</h2>
+      <button @click="cleanTodo">Clean</button>
+      <ul>
+        <li v-for="t in doneTodo" :key="t.name">
+          <v-row align="center">
+            <v-col class="d-flex" cols="12" sm="4">
+              <v-select
+                :items="items"
+                v-model="t.status"
+                item-text="label"
+                item-value="value"
+                label="Status"
+                return-object
+                @change="changeStatus(t, $event)"
+              ></v-select>
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="4">
+              <!-- <input type="checkbox" v-model="t.state===''" /> -->
+              {{ t.name }}
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="4">
+              <button @click="deleteTodo(t)">削除</button>
+            </v-col>
+          </v-row>
+        </li>
+      </ul>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -30,7 +95,12 @@ export default {
   name: "List",
   data() {
     return {
-      newText: ""
+      newText: "",
+      items: [
+        { label: "ToDo", value: "todo" },
+        { label: "Done", value: "done" },
+        { label: "Doing", value: "doing" }
+      ]
     };
   },
   methods: {
@@ -43,17 +113,29 @@ export default {
     },
     cleanTodo() {
       this.$store.commit("cleanTodo");
+    },
+    changeStatus(task, event) {
+      // console.log(event)
+      // task.status = event.value
+      // console.log(task)
+      this.$store.commit("changetaskstate", {
+        target: task.name,
+        status: event.value
+      });
     }
   },
   computed: {
-    todos() {
-      return this.$store.state.todos;
+    tasks() {
+      return this.$store.state.tasks;
     },
-    notDoneTodo() {
-      return this.$store.getters.notDoneTodo;
+    todo() {
+      return this.$store.getters.todo;
     },
     doneTodo() {
       return this.$store.getters.doneTodo;
+    },
+    doingTodo() {
+      return this.$store.getters.doingTodo;
     }
   }
 };
